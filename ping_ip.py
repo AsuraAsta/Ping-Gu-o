29c7bbded00119295634f8fd385b9ab985896842
João Pedro <joaopalheiros.pf@gmail.com>
2026-04-21 17:48:22 -0300

    Ajustes feitos no PC

--- ping_ip.py
+++ ping_ip.py
@@ -1,9 +1,16 @@
-import subprocess 
+import subprocess  
 import platform
 
+#Subprocess ele tem uam funcao de abrir o cmd ou terminal e rodar um comando nele sem que o user veja
+#Já o Platform ele tem uma funcao para identificar em ql sistema eu estou
+
+ip = "8.8.8.8"
 def ping_ip (ip):
-    sistema = platform.system()
-    print(sistema)
-    
-    
-ping_ip("8.8.8.8")
+    sistema = platform.system()  #Declarei a variavel sistemas e disse a pra busca pelo platforma.system em qual sistema op eu estou 
+    if sistema == "Windows":
+        parametro = "-n"
+    else: 
+        parametro = "-c"   
+    comando = ["ping", parametro, "1", ip] #Declarei essa variavel para poder passar os termos que vao ser jogados no cmd .
+    resultado = subprocess.run(comando, capture_output=True) #Variavel vai jopgar no cmd via subprocess os tem na var comando e vai capturar oq sair de la pelo capture_output
+    print(resultado)
